ca73201b5f7b93a53e899f04f0c25b954218b380
Leonardo Cruciani <50495467+Naarsk@users.noreply.github.com>
2019-07-07 12:30:40 +0200

    Update interpolante.py
    interpolante trilineare, i valori f(x0[l+i], y0[m+j], z0[n+k]) sono quelli da tabulare

--- interpolante.py
+++ interpolante.py
@@ -1,43 +1,85 @@
 import math
-def f(x,y,z):
+
+def f(x,y,z):                                           #function to be estimated
    r=x*math.exp(-x**2-y**2-z**2)
    return r
 
-def g(x,y,z,x0,y0,z0):
+def g(x,y,z,x0,y0,z0):                                  #trilinear interpolating function
+    
     r=0.0
+    
     i=0
-    l=0
-    while (l<100):#per ora con l cosi si trovano solo i punti (a,a,a) sulla diagonale del cubo-dominio, sistema poi con piu indici indipendenti l,m,
-        if (x>x0[l] and y>y0[l] and z>z0[l] and x<=x0[l+1] and y<=y0[l+1] and z<=z0[l+1]):
-            i=0
-            while (i<2):
-                j=0
-                while (j<2):
-                    k=0       
-                    while (k<2):        
-                        r=r+(-1)**(i+j+k-1)*(x-x0[l+abs(i-1)])*(y-y0[l+abs(j-1)])*(z-z0[l+abs(k-1)])*f(x0[l+i],y0[l+j],z0[l+k])
-                        k=k+1
-                    j=j+1
-                i=i+1
-            r=r/((x0[l+1]-x0[l])*(y0[l+1]-y0[l])*(z0[l+1]-z0[l]))
-        l=l+1
-    return r1
+    while (i<d1):                                       
+        if (x>x0[i] and  x<=x0[i+1]):                   #pick the x interval
+            l=i                                         #define l
+            break
+        i=i+1
+    
+    i=0        
+    while(i<d2):                                        
+        if(y>y0[i] and y<=y0[i+1]):                     #pick the y interval
+            m=i                                         #define m
+            break
+        i=i+1
+    
+    i=0
+    while(i<d3):
+        if(z>z0[i] and z<=z0[i+1]):                     #pick the z interval
+            n=i                                         #define n
+            break
+        i=i+1
+                                                    
+    i=0
+    while (i<2):
+        j=0
+        while (j<2):
+            k=0       
+            while (k<2):                                
+                r=r+(-1)**(i+j+k-1)*(x-x0[l+abs(i-1)])*(y-y0[m+abs(j-1)])*(z-z0[n+abs(k-1)])*f(x0[l+i],y0[m+j],z0[n+k]) 
+                k=k+1                                   #f values should be tabulated in a list: F[l+i,m+j,n+k]
+            j=j+1
+        i=i+1
+    r=r/((x0[l+1]-x0[l])*(y0[m+1]-y0[m])*(z0[n+1]-z0[n]))
+    return r
+    
+a=1.05                                                  #point to estimate the function in
+b=1.35
+c=0.72
 
-X0=[0]*101
-Y0=[0]*101    
-Z0=[0]*101
+d1=100                                                  #no. of divisions of dominium axis
+d2=200
+d3=150
 
-l=0
-while (l<101):
-    X0[l]= -2+l*0.04
-    Y0[l]= -2+l*0.04 #l to m
-    Z0[l]= -2+l*0.04 #l to n
-    l=l+1
+o1=0                                                    #lower extremum of dominium axis
+o2=0
+o3=0
 
-l=0
-while (l<101):
-    a=0.285+0.005*l
-  #  print(f(a,a,a))
-  #  print(g(a,a,a,X0,Y0,Z0))
-    print(100*abs(f(a,a,a)-g(a,a,a,X0,Y0,Z0))/f(a,a,a),"%")
-    l=l+1
+s1=4                                                    #upper extremum of dominium axis
+s2=6
+s3=3
+
+w1=(s1-o1)/d1                                           #interval width
+w2=(s2-o2)/d2 
+w3=(s3-o3)/d3 
+
+X0=[0]*(d1+1)                                           #the grid 
+Y0=[0]*(d2+1) 
+Z0=[0]*(d3+1)
+
+i=0
+while (i<=d1):
+    X0[i]= o1+w1*i
+    i=i+1
+i=0
+while (i<=d2):
+    Y0[i]= o2+w2*i
+    i=i+1
+i=0
+while (i<=d3):
+    Z0[i]= o3+w3*i
+    i=i+1
+    
+
+print("real function value:", f(a,b,c))
+print("iterpolated value:", g(a,b,c,X0,Y0,Z0))
+print("percentage error:", round(100*abs(f(a,b,c)-g(a,b,c,X0,Y0,Z0))/f(a,b,c),2),"%")
